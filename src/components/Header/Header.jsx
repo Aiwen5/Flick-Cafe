bc020b996458437bee3ab588818acd6196ac891a
shirinf <sforouzan@my.bcit.ca>
2024-12-10 19:46:22 -0800

    cart overlay functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import { FaShoppingBag, FaUser } from 'react-icons/fa';
+import CartOverlay from '../Menu/CartOverlay';
 
-export default function Header({setOverlayOpen}) {
+export default function Header({overlayCategories, setOverlayCategories}) {
+  const [overlayOpen, setOverlayOpen] = useState(false);
+  const [itemQtyState, setItemQtyState] = useState();
+
+  useEffect(() => {
+    const existingCategories = JSON.parse(localStorage.getItem("cartItems")) || {};
+    setOverlayCategories(existingCategories)
+  }, [setOverlayCategories])
+  
+  useEffect(() => {
+    if (overlayOpen) {
+      document.body.classList.add("overlay-open");
+    } else {
+      document.body.classList.remove("overlay-open");
+    }
+
+    return () => {
+      document.body.classList.remove("overlay-open");
+    };
+  }, [overlayOpen]);
   return (
     <header className={styles.header}>
       <Link to="/" className={styles.logo}>
@@ -19,9 +40,16 @@ export default function Header({setOverlayOpen}) {
       <div className={styles.icons}>
         <button className={styles.icon} onClick={() => setOverlayOpen(true)}>
           <FaShoppingBag />
+          {!!itemQtyState && (
+            <div className={styles.indicator}>
+              {itemQtyState}
+            </div>
+          )}
         </button>
         <FaUser className={styles.icon} />
+        
       </div>
+      <CartOverlay cartItems={overlayCategories} {...{setOverlayCategories, setOverlayOpen, overlayOpen, setItemQtyState}} />
     </header>
   );
 }
--- src/components/Menu/CartOverlay.jsx
+++ src/components/Menu/CartOverlay.jsx
@@ -1,11 +1,19 @@
 import styles from "./CartOverlay.module.css";
 import { useState, useEffect } from "react";
+import { FaMinus, FaPlus } from 'react-icons/fa';
 
-const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
+const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen, setItemQtyState, setOverlayCategories}) => {
   const [menuData, setMenuData] = useState(null);
   const [subTotal, setSubTotal] = useState(0);
 
-  console.log('cart items prop', Object.keys(cartItems).length)
+  useEffect(() => {
+    const existingCategories = JSON.parse(localStorage.getItem("cartItems")) || {};
+  
+    const updatedCategories = { ...existingCategories, ...cartItems };
+  
+    localStorage.setItem("cartItems", JSON.stringify(updatedCategories));
+  
+  }, [cartItems]);
 
   useEffect(() => {
     const fetchMenuData = async () => {
@@ -25,9 +33,13 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
   }, []);  
 
   const updateSubtotal = (item) => {
+    let itemQty = Object.values(item).reduce(
+      (acc, entry) => acc + entry.quantity, 0
+    );
     const newSubTotal = Object.values(item).reduce(
       (acc, entry) => acc + entry.quantity * entry.price, 0
     )
+    setItemQtyState(itemQty);
     setSubTotal(newSubTotal)
     localStorage.setItem("subTotal", newSubTotal);
   }
@@ -40,16 +52,14 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
     const updatedCartItems = { ...cartItems };
   
     if (newQuantity === 0) {
-      console.log('quantity is 0')
       delete updatedCartItems[id];  
+      setOverlayCategories(updatedCartItems)
     } else {
       updatedCartItems[id].quantity = newQuantity;
     }
 
-    console.log('updated cart items', updatedCartItems)
     updateSubtotal(updatedCartItems);
     localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
-    console.log('local storage cart items:', localStorage.getItem('cartItems'))
   };
 
   const OrderItem = ({ item, quantity, updateStorage }) => {
@@ -66,8 +76,8 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
           </div>
           <div className={styles.right}>
             <div className={styles.text}>
-              <p>{title}</p>
-              <p>${(Math.round(price * 100) / 100).toFixed(2)}</p>
+              <p className={styles.productTitle}>{title}</p>
+              <p className={styles.productPrice}>${(Math.round(price * 100) / 100).toFixed(2)}</p>
             </div>
             <div className={styles.quantity}>
               <button
@@ -75,10 +85,10 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
                   setQuantityState((prev) => (prev - 1))
                 }
               >
-                -
+                <FaMinus color="#fff" />
               </button>
               {quantityState}
-              <button onClick={() => setQuantityState((prev) => prev + 1)}>+</button>
+              <button onClick={() => setQuantityState((prev) => prev + 1)}><FaPlus color="#fff" /></button>
             </div>
           </div>
         </div>
@@ -88,10 +98,11 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
 
   return (
     <div className={`${styles.cartOverlay} ${overlayOpen && styles.open}`}>
+      <div>
       <div className={styles.top}>
         <div className={styles.row}>
         <h2>Order</h2>
-        <button onClick={() => setOverlayOpen(false)}>X</button>
+        <button onClick={() => setOverlayOpen(false)}><svg width={20} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">{/*<!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->*/}<path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" fill="white" /></svg></button>
         </div>
         {!!Object.keys(cartItems).length ? (
           <p>Pick-up in store</p>
@@ -99,25 +110,28 @@ const CartOverlay = ({cartItems, setOverlayOpen, overlayOpen}) => {
           <p>Your order is currently empty.</p>
         )}
       </div>
-      <div className={styles.cartItems}>
-      {menuData && Object.keys(cartItems).map((id) => {
-  
-        for(const category in menuData){
-          const item = menuData[category].items.find((item) => item.id === id);
-
-          if(item){
-            return <OrderItem key={`item-${id}`} quantity={cartItems[id].quantity} {...{ item, updateStorage }} />;
-          }
-        }
-        
-      })}
-      </div>
+      {!!Object.keys(cartItems).length && (
+        <div className={styles.cartItems}>
+          {menuData && Object.keys(cartItems).map((id) => {
+            for(const category in menuData){
+              const item = menuData[category].items.find((item) => item.id === id);
+              if(item){
+                return <OrderItem key={`item-${id}`} quantity={cartItems[id].quantity} {...{ item, updateStorage }} />;
+              }
+            }
+          })}
+        </div>
+      )}
       
+      </div>
+  
       {!!Object.keys(cartItems).length && (
       <div className={styles.bottom}>
-        
-        <p>${(Math.round(subTotal * 100) / 100).toFixed(2)}</p>
-        <button>Checkout</button>
+        <div className={styles.row}>
+          <p>Subtotal</p>
+          <p>${(Math.round(subTotal * 100) / 100).toFixed(2)}</p>
+        </div>
+        <a href="/cart">CHECKOUT</a>
       </div>
       )}
     </div>
